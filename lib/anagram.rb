ff5e5727d7b8fd2d3852653e32903106829ab7f6
Pe-ngo <jamespengo95@gmail.com>
2022-09-02 23:25:45 +0300

    Completed Assignment

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,27 @@
 # Your code goes here!
+class Anagram 
+
+    attr_accessor :close 
+
+    def initialize(close) 
+        @close = close 
+    end 
+
+    def match(array)
+        
+        anagram = []
+        array.each do |item|
+            if @close.chars.sort==item.chars.sort 
+                anagram << item 
+            end
+        end
+        anagram
+    end
+end
+
+
+
+
+# def match(array)
+#     array.select {|x| x.split("").sort == @close.split("").sort}
+# end   
